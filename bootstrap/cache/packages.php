ef8e9669dddf98bea2697d46e0a5dea5e451836b
Emmanuel Obute <emmanuel6.obute@gmail.com>
2022-01-25 00:51:51 +0100

    After Git issue

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,4 +1,11 @@
 <?php return array (
+  'coconutcraig/laravel-postmark' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Coconuts\\Mail\\PostmarkServiceProvider',
+    ),
+  ),
   'facade/ignition' => 
   array (
     'providers' => 
@@ -68,6 +75,18 @@
     'providers' => 
     array (
       0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'tymon/jwt-auth' => 
+  array (
+    'aliases' => 
+    array (
+      'JWTAuth' => 'Tymon\\JWTAuth\\Facades\\JWTAuth',
+      'JWTFactory' => 'Tymon\\JWTAuth\\Facades\\JWTFactory',
+    ),
+    'providers' => 
+    array (
+      0 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
     ),
   ),
 );
