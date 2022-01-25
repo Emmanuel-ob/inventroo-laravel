6ee87a4aef5fcf1f0a2329bf3a246158abce59f4
Emmanuel Obute <emmanuel6.obute@gmail.com>
2022-01-25 01:21:34 +0100

    After Git issue

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group([
+    'namespace' => 'App\Http\Controllers\AppService',
+    'prefix' => 'api',
+   ], function() {
+
+
+    Route::post('register', 'UserController@createAccount');
+    Route::post('login', 'UserController@login');
+    
+    Route::post('password/resetByEmail', 'UserController@resetPasswordByEmail');
+    Route::post('password/resetConfirm', 'UserController@resetPasswordConfirm');
+    
+    Route::get('account/confirm', 'UserController@confirmAccount');
+    
+
+   Route::group([
+    'middleware' => 'AuthToken',
+    
+   ], function() {
+        Route::post('logout', 'UserController@logout');
+        Route::post('refreshToken', 'UserController@refreshToken');
+        Route::post('resetPassword', 'UserController@resetPassword');
+
+     });
+
+
+});
