c4619cb1a3fedef9cdadcf0b709d8a6c71902742
Emmanuel Obute <emmanuel6.obute@gmail.com>
2022-03-05 07:57:50 +0100

    Product Group routines

--- app/Http/Controllers/AppService/ProductController.php
+++ app/Http/Controllers/AppService/ProductController.php
@@ -1138,6 +1138,8 @@ class ProductController extends Controller
         if (!$user) {
            return response()->json(["ResponseStatus" => "Unsuccessful", 'Detail' => 'User not found.', "ResponseMessage" => "User not found.", "ResponseCode" => 401], 401);
         }
+        $attributes = $request->input('attributes');
+        return response()->json(compact('attributes'),201);
         if ($user->account_type != '') {
           $productGr = ProductGroup::find($request->input('productGroupID'));
           $productGr_bk = $productGr;
